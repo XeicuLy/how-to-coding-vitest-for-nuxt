bb22df8538d7ad38e01a60b9cf4b5064757e3381
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-03-08 14:27:28 +0900

    :wrench: chore: gitignoreファイルに設定されたファイル郡を無視する

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,11 @@
 // @ts-check
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { includeIgnoreFile } from '@eslint/compat';
 import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
+
+export default withNuxt([includeIgnoreFile(gitignorePath)]);
