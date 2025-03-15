dd5095c233d32ef4c8799c9998545d9de51e71ec
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-03-16 00:46:06 +0900

    :hammer: chore: uiモードでcoverageを確認する

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,12 +2,19 @@ import { defineVitestConfig } from '@nuxt/test-utils/config';
 
 export default defineVitestConfig({
   test: {
+    globals: true,
     environment: 'nuxt',
     reporters: ['verbose'],
     environmentOptions: {
       nuxt: {
         domEnvironment: 'happy-dom',
       },
+    },
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: '../logs/coverage',
+      all: true,
     },
   },
 });
