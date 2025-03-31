e5d151cb6a77054da3345648d7d61f1a3016fae2
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-03-31 21:13:10 +0900

    :white_check_mark: test: 外部依存の関数のテストを追加
    ISSUES CLOSED: #40

--- src/__test__/components/ChildComponent.nuxt.spec.ts
+++ src/__test__/components/ChildComponent.nuxt.spec.ts
@@ -1,24 +1,37 @@
-import { describe, expect, test } from 'vitest';
+import { describe, expect, test, vi } from 'vitest';
 import ChildComponent from '@/components/ChildComponent.vue';
 import { mountSuspendedComponent } from '@/helpers/test';
 
+vi.mock('@/utils/logger.ts', () => ({
+  logger: vi.fn(),
+}));
+
 describe('src/components/ChildComponent.vue', () => {
-  describe('Propsテスト', () => {
-    test('親コンポーネントから受け取ったPropsの値が適切に表示されるか', async () => {
-      const props = {
-        textContent: 'Test Props',
-      };
-      const wrapper = await mountSuspendedComponent(ChildComponent, { props });
+  test('親コンポーネントから受け取ったPropsの値が適切に表示されるか', async () => {
+    const props = {
+      textContent: 'Test Props',
+    };
+    const wrapper = await mountSuspendedComponent(ChildComponent, { props });
 
-      expect(wrapper.exists()).toBe(true);
-      expect(wrapper.find('#child-component').text()).toBe('Test Props');
-    });
+    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.find('#child-component').text()).toBe('Test Props');
+  });
 
-    test('親コンポーネントからPropsが渡されない場合、デフォルト値が表示されるか', async () => {
-      const wrapper = await mountSuspendedComponent(ChildComponent);
+  test('親コンポーネントからPropsが渡されない場合、デフォルト値が表示されるか', async () => {
+    const wrapper = await mountSuspendedComponent(ChildComponent);
 
-      expect(wrapper.exists()).toBe(true);
-      expect(wrapper.find('#child-component').text()).toBe('Default Text');
-    });
+    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.find('#child-component').text()).toBe('Default Text');
+  });
+
+  test('ボタンをクリックしたときに外部で定義したイベントが正しく発火するか', async () => {
+    const wrapper = await mountSuspendedComponent(ChildComponent);
+    const target = wrapper.find('button');
+
+    expect(target.exists()).toBe(true);
+
+    await target.trigger('click');
+
+    expect(logger).toHaveBeenCalledTimes(1);
   });
 });
